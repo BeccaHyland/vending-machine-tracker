ca18413eba7ae65096e69584c47e4033d96f99ca
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-07 10:59:43 -0600

    ATP for User Story 2, adds snack_avg to machine show spec and view

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -10,19 +10,20 @@ feature 'When a user visits a vending machine show page' do
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
 
-  scenario 'they see the machine snack names and prices' do
+  scenario 'they see the machine snack names, prices, and average snack price' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
     snack_1 = dons.snacks.create(name: "Skittles", price: 1)
-    snack_2 = dons.snacks.create(name: "zebra cakes", price: 2)
+    snack_2 = dons.snacks.create(name: "zebra cakes", price: 3)
     snack_3 = Snack.create(name: "pop tart", price: 3)
 
-    visit machine_path(dons)    
+    visit machine_path(dons)
 
     expect(page).to have_content(snack_1.name)
     expect(page).to have_content(snack_1.price)
     expect(page).to have_content(snack_2.name)
     expect(page).to have_content(snack_2.price)
+    expect(page).to have_content("Average Snack Price for this Machine: 2")
     expect(page).to_not have_content(snack_3.name)
   end
 end
