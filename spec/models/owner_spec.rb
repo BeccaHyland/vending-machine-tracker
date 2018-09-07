66bbd0934258fa4331189cb4b646bc831a68fdb6
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-07 10:28:22 -0600

    corrects model spec contents from master error

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
-describe Machine, type: :model do
+describe Owner, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :location }
-    it { should belong_to :owner }
-    it { should have_many :machine_snacks }
-    it { should have_many :snacks, through: :machine_snacks }
-  end
+    it { should validate_presence_of :name }
+    it { should have_many :machines }
   end
 end
