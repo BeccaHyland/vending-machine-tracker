3bbf578e36766bf8e7868e2387cb1801cba47002
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-07 10:18:25 -0600

    initial commit bundles Gemfile, drafts spec test for machine show

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,20 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see the machine snack names and prices' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = dons.snacks.create(name: "Skittles", price: 1)
+    snack_2 = dons.snacks.create(name: "zebra cakes", price: 2)
+    snack_3 = Snack.create(name: "pop tart", price: 3)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content(snack_1.name)
+    expect(page).to have_content(snack_1.price)
+    expect(page).to have_content(snack_2.name)
+    expect(page).to have_content(snack_2.price)
+    expect(page).to_not have_content(snack_3.name)
+  end
 end
