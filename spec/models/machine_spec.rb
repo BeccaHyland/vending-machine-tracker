46835ae836ca4c76bb23afaf130d504f739295bd
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-07 10:57:33 -0600

    ATP MAchine class method snack_avg

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -6,4 +6,15 @@ describe Machine, type: :model do
     it { should belong_to :owner }
     it { should have_many(:snacks).through(:machine_snacks)}
   end
+
+  describe 'class methods' do
+    it 'calculates average snack price in one machine' do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      snack_1 = dons.snacks.create(name: "Skittles", price: 1)
+      snack_2 = dons.snacks.create(name: "zebra cakes", price: 3)
+
+      expect(dons.snack_avg).to eq(2)
+    end
+  end
 end
